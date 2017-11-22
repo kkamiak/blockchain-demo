318638a422695114bf3bfd43f6a508bcd1b183cf
Belko <siarhei.belko@instinctools.ru>
2017-11-22 08:30:27 +0300

    add etheriumJ lib

--- src/main/java/by/instinctools/TransactionDto.java
+++ src/main/java/by/instinctools/TransactionDto.java
@@ -1,16 +1,48 @@
 package by.instinctools;
 
-import org.web3j.crypto.RawTransaction;
-
 import java.io.Serializable;
 
-/**
- * Created by haria on 21.11.17.
- */
-public class TransactionDto implements Serializable{
+public class TransactionDto implements Serializable {
 
+    private static final long serialVersionUID = -612398939922696830L;
+
+    private String tx;
+    private String from;
+    private String to;
+    private String gasLimit;
     private String token;
-    private RawTransaction tx;
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getTx() {
+        return tx;
+    }
+
+    public void setTx(String tx) {
+        this.tx = tx;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getGasLimit() {
+        return gasLimit;
+    }
+
+    public void setGasLimit(String gasLimit) {
+        this.gasLimit = gasLimit;
+    }
 
     public String getToken() {
         return token;
@@ -18,13 +50,5 @@ public class TransactionDto implements Serializable{
 
     public void setToken(String token) {
         this.token = token;
-    }
-
-    public RawTransaction getTx() {
-        return tx;
-    }
-
-    public void setTx(RawTransaction tx) {
-        this.tx = tx;
     }
 }
--- src/main/java/by/instinctools/controller/BlockchainController.java
+++ src/main/java/by/instinctools/controller/BlockchainController.java
@@ -1,46 +1,39 @@
 package by.instinctools.controller;
 
 import by.instinctools.TransactionDto;
+import org.apache.commons.lang3.StringUtils;
+import org.ethereum.core.Transaction;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.web3j.crypto.RawTransaction;
-import org.web3j.protocol.core.methods.request.Transaction;
-import org.web3j.utils.Convert;
-import org.web3j.utils.Numeric;
 
 import java.util.logging.Logger;
 
-/**
- * Created by haria on 21.11.17.
- */
 @Controller
-@RequestMapping(value="/api")
+@RequestMapping(value = "/api")
 public class BlockchainController {
 
-    public static final  Logger LOGGER = Logger.getLogger(BlockchainController.class.getName());
+    public static final Logger LOGGER = Logger.getLogger(BlockchainController.class.getName());
+
+    public BlockchainController(@Value("smart.contract.address") final String smartContractAddress) {
+        this.smartContractAddress = smartContractAddress;
+    }
+
+    private final String smartContractAddress;
 
     @PostMapping(path = "/blockchain")
     public void sendTransaction(@RequestBody final TransactionDto transaction) {
-        final RawTransaction tx = transaction.getTx();
-//
-//        final String data = tx.getData();
-//        final String transactionSigner = tx.getFrom().toLowerCase();
-//        final String recipient = tx.getTo().toLowerCase();
-//        final String value = tx.getValue();
-//        final String gasLimit = tx.getGas();
-//        final String gasPrice = tx.getGasPrice();
-//        final String nonce = tx.getNonce();
-//
-//        LOGGER.info("Signed by: " + transactionSigner);
-//        LOGGER.info("Nonce: " + nonce);
-//        LOGGER.info("To: " + recipient );
-//        LOGGER.info("Value: " + value + " (" + Convert.fromWei(value, Convert.Unit.ETHER) + "ETH)");
-//        LOGGER.info("Data: " + Numeric.toHexString(data.getBytes()));
-//        LOGGER.info("Gas limit: " + gasLimit);
-//        LOGGER.info("Gas Price: " + gasPrice + " (" + Convert.fromWei(gasPrice, Convert.Unit.GWEI) + "Gwei)");
+
+        final String data = transaction.getTx();
+        final String transactionSigner = transaction.getFrom();
+        final String gasLimit = transaction.getGasLimit();
+
+        if (StringUtils.equals(transaction.getTo(), smartContractAddress)) {
+//            throw  new IllegalArgumentException('Wrong contract address');
+//            return next(error);
+        }
 
     }
-
 }
