652e8abaf1233ea9c72f2745bb3f081aaca1147a
haria <kanstansin.kamiak@instinctools.ru>
2017-11-22 17:16:53 +0300

    Added connection configuration for rinkeby

--- src/main/java/by/instinctools/controller/BlockchainController.java
+++ src/main/java/by/instinctools/controller/BlockchainController.java
@@ -1,14 +1,22 @@
 package by.instinctools.controller;
 
 import by.instinctools.TransactionDto;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.apache.commons.lang3.StringUtils;
 import org.ethereum.core.Transaction;
+import org.ethereum.facade.Ethereum;
+import org.ethereum.facade.EthereumFactory;
+import org.ethereum.util.blockchain.EtherUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.math.BigInteger;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.util.logging.Logger;
 
 @Controller
@@ -24,16 +32,31 @@ public class BlockchainController {
     private final String smartContractAddress;
 
     @PostMapping(path = "/blockchain")
-    public void sendTransaction(@RequestBody final TransactionDto transaction) {
+    public void sendTransaction(@RequestBody final TransactionDto transaction) throws DecoderException {
 
         final String data = transaction.getTx();
-        final String transactionSigner = transaction.getFrom();
-        final String gasLimit = transaction.getGasLimit();
+        byte[] b = Hex.decodeHex(data.toCharArray());
 
-        if (StringUtils.equals(transaction.getTo(), smartContractAddress)) {
-//            throw  new IllegalArgumentException('Wrong contract address');
-//            return next(error);
-        }
+        Transaction tx = new Transaction(b);
+        tx.rlpParse();
+
+        BigInteger nonce = new BigInteger(tx.getNonce());
+        BigInteger value = new BigInteger(tx.getValue());
+        BigInteger gasLimit = new BigInteger(tx.getGasLimit());
+        BigInteger gasPrice = new BigInteger(tx.getGasPrice());
+        String transactionSigner = Hex.encodeHexString(tx.getSender());
+        String receiver = Hex.encodeHexString(tx.getReceiveAddress());
+
+        LOGGER.info("Signed by: " + transactionSigner);
+        LOGGER.info("Nonce" + nonce);
+        LOGGER.info("To" + receiver);
+        LOGGER.info("Value" + value + " (" + EtherUtil.convert(value.longValue(), EtherUtil.Unit.ETHER) + " ETH)");
+        LOGGER.info("Data: " + data);
+        LOGGER.info("Gas limit: " + gasLimit);
+        LOGGER.info("Gas price: " + gasPrice + " (" + EtherUtil.convert(gasPrice.longValue(), EtherUtil.Unit.GWEI) +  " Gwei)");
+
+        Ethereum ethereum = EthereumFactory.createEthereum();
+
 
     }
 }
