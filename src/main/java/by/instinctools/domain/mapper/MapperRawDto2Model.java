bd34a0521fa0dcc1a5df1f1da154da2b8e576f11
Belko <siarhei.belko@instinctools.ru>
2017-11-27 10:51:54 +0300

    implement mapper logic

--- src/main/java/by/instinctools/domain/mapper/MapperRawDto2Model.java
+++ src/main/java/by/instinctools/domain/mapper/MapperRawDto2Model.java
@@ -1,11 +1,13 @@
 package by.instinctools.domain.mapper;
 
 import by.instinctools.rest.dto.TransactionDto;
+import by.instinctools.utils.Transaction;
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.stereotype.Service;
 import org.web3j.crypto.RawTransaction;
 
-import static java.math.BigInteger.ZERO;
-import static org.apache.commons.lang3.StringUtils.EMPTY;
+import java.math.BigInteger;
 
 @Service
 public class MapperRawDto2Model implements MapperManagement<TransactionDto, RawTransaction> {
@@ -13,34 +15,23 @@ public class MapperRawDto2Model implements MapperManagement<TransactionDto, RawT
     @Override
     public RawTransaction transform(final TransactionDto transaction) {
 
-//        TODO: stub method. waiting to implement decoding string
-//        final String data = transaction.getTx();
-//        byte[] b = Hex.decodeHex(data.toCharArray());
-//
-//
-//        BigInteger nonce = new BigInteger(tx.getNonce());
-//        BigInteger value = new BigInteger(tx.getValue());
-//        BigInteger gasLimit = new BigInteger(tx.getGasLimit());
-//        BigInteger gasPrice = new BigInteger(tx.getGasPrice());
-//        String transactionSigner = Hex.encodeHexString(tx.getSender());
-//        String receiver = Hex.encodeHexString(tx.getReceiveAddress());
-//
-//        LOGGER.info("Signed by: " + transactionSigner);
-//        LOGGER.info("Nonce " + nonce);
-//        LOGGER.info("To " + receiver);
-//        LOGGER.info("Value " + value + " (" + EtherUtil.convert(value.longValue(), EtherUtil.Unit.ETHER) + " ETH)");
-//        LOGGER.info("Data: " + data);
-//        LOGGER.info("Gas limit: " + gasLimit);
-//        LOGGER.info("Gas price: " + gasPrice + " (" + EtherUtil.convert(gasPrice.longValue(), EtherUtil.Unit.GWEI) + " Gwei)");
+        try {
 
-        return RawTransaction.createTransaction(
-                ZERO,
-                ZERO,
-                ZERO,
-                EMPTY,
-                ZERO,
-                EMPTY
-        );
+            final String data = transaction.getTx();
+            final Transaction tx = new Transaction(Hex.decodeHex(data.toCharArray()));
+
+            return RawTransaction.createTransaction(
+                    new BigInteger(tx.getNonce()),
+                    new BigInteger(tx.getGasPrice()),
+                    new BigInteger(tx.getGasLimit()),
+                    Hex.encodeHexString(tx.getReceiveAddress()),
+                    new BigInteger(tx.getValue()),
+                    Hex.encodeHexString(tx.getData())
+            );
+
+        } catch (DecoderException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 
 }
